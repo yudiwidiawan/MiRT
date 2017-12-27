15264d5da4ca679bcadb26644fd19cdcf12c8b79
debi dwitama yusup <dwittamma19@gmail.com>
2017-12-27 15:00:07 +0700

    menambahkan form tambah warga

--- app/src/main/java/bif14/mirt/MainActivity.java
+++ app/src/main/java/bif14/mirt/MainActivity.java
@@ -45,14 +45,14 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, TambahWarga.class);
+                startActivity(intent);
             }
-        });*/
+        });
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
